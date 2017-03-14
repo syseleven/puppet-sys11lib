030f0813db990a9c93bb257b3787a26568c96f45
GitLab <puppetmaster@syseleven.de>
2017-03-14 14:33:20 +0100

    Automatic split repo commit

--- manifests/ve_name.pp
+++ manifests/ve_name.pp
@@ -4,15 +4,10 @@
 #   None
 #
 class sys11lib::ve_name () {
-
-  # touch only if sys11name/role is set (and not when running puppet locally), pp-1155
-  if $::role != undef or $::roles != undef {
-    file { '/.ve-name':
-      mode    => '0400',
-      owner   => root,
-      group   => root,
-      content => template('sys11lib/ve_name.erb'),
-    }
+  file { '/.ve-name':
+    mode    => '0400',
+    owner   => root,
+    group   => root,
+    content => template('sys11lib/ve_name.erb'),
   }
-
 }
